bf41dd4ff3edb16e2380eeddde0358bdab439d2c
Lakshya Saharan <lakshyasaharan@gmail.com>
2026-02-15 18:01:32 -0500

    playwright basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-console.log("Hello World");
-console.log("Hello World");
+import { chromium } from 'playwright';
+
+const launchBrowser = async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 1000 });
+
+  const page = await browser.newPage();
+  await page.goto('file:///Users/lakshyasaharan/projects/stagehand-lite/examples/company-site/index.html');
+
+  console.log(await page.locator('//html/body').ariaSnapshot());
+  await page.getByRole('link', { name: 'About Us' }).click();
+
+  await browser.close();
+};
+
+launchBrowser();
+
